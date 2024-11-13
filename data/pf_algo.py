85d32f291ea0d190e038d67ceabc1f9eb7f4769c
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2024-11-13 16:02:55 -0600

    ParticleFilter2 start

--- data/pf_algo.py
+++ data/pf_algo.py
@@ -167,8 +167,7 @@ class ParticleFilter1:
         weights = self.particles[:,W]
         neff = 1. / np.sum(np.square(weights))
         # print(f" N effective particles {neff}")
-        threshold = self.N/5
-
+        threshold = self.N/2
         # At a certain point we stop re-sampling?
         return neff < threshold
     
@@ -177,16 +176,15 @@ class ParticleFilter1:
         # add more noise the more curvature there is because we are less certain of our answer <- this strat works better it seems
         # or add more noise with less curvature, so we can recover closer to the GT line.
 
-        # PARTICLE_CEIL = 100 # This has to be high enough to tweak the estimate towards GT, but not high enough to make the estimate random
+        PARTICLE_CEIL = 100 # This has to be high enough to tweak the estimate towards GT, but not high enough to make the estimate random
+        TURN_CEIL = 0.10745999999999996
+        curve_ratio = (seg_curvature/TURN_CEIL)**2
+        print(f"Curve ratio {curve_ratio}")
 
-        # TURN_CEIL = 0.10745999999999996
-        # curve_ratio = (seg_curvature/TURN_CEIL)
-        # print(f"Curve ratio {curve_ratio}")
+        PARTICLE_CEIL *= curve_ratio
+        PARTICLE_CEIL = int(PARTICLE_CEIL)
 
-        # PARTICLE_CEIL *= curve_ratio
-        # PARTICLE_CEIL = int(PARTICLE_CEIL)
-
-        # print(f"Noise particles: {PARTICLE_CEIL}")
+        print(f"Noise particles: {PARTICLE_CEIL}")
 
         print("Pre-resample")
         self.show_particles()
@@ -198,10 +196,7 @@ class ParticleFilter1:
 
         # resample according to indexes
         self.particles[:] = self.particles[indexes]
-
-        # self.particles = next_particles
-        self.particles[:,W] = 1.0/ self.N 
-
+        self.particles[:,W] = 1.0/self.N 
         
         # Once all particles weigh the same, we add noise to the data distribution
         # by randomly moving / re-orienting 100 particles.
@@ -215,8 +210,6 @@ class ParticleFilter1:
             # self.particles[i,X] += r * np.cos(theta)
             # self.particles[i,Y] += r * np.sin(theta)
             self.particles[i,O] += random.uniform(-max_turn , +max_turn)
-
-        # self.norm_particles()
 
         print("Post-resample")
         self.show_particles()
@@ -250,6 +243,113 @@ class ParticleFilter1:
         for i in range(self.N):
             self.particles[i, W] = 1./self.N
 
+class ParticleFilter2:
+    def __init__(self, n_particles):
+        self.pose = None
+        self.N = n_particles
+        self.particles = np.zeros((n_particles, 4)) # n_particles rows, 4 columns
+        self.particles_over_t = []
+
+    def norm_particles(self):
+        self.particles[:,W] = self.particles[:,W] / np.sum(self.particles[:,W])
+
+    def generate(self, start_pose):
+        r_dist = 0.5
+        max_turn = np.pi / 8
+
+        # Create states at uniform, and weight them according to 3D Gaussian
+        for i in range(self.N):
+            r = random.uniform(0, r_dist)
+            theta = random.uniform(0, 2*np.pi)
+            self.particles[i,X] = start_pose.x + r * np.cos(theta)
+            self.particles[i,Y] = start_pose.y + r * np.sin(theta)
+            self.particles[i,O] = start_pose.orientation + random.uniform(-max_turn , +max_turn)
+
+        var_x = np.var( self.particles[:,X], axis=0)
+        var_y = np.var( self.particles[:,Y], axis=0)
+        var_o = np.var( self.particles[:,O], axis=0)
+        # So for NUMPY, you actually have to use the [ , ] in all scenarios for proper slicing
+
+        mean_state = np.mean(self.particles[:,:W], axis=0)
+        variances = np.array([var_x, var_y, var_o])
+        covariance = np.diag(variances) # Create a cov matrix assuming no cross-correlations
+
+        for i in range(self.N):
+            self.particles[i,W] =  multivariate_normal.pdf(self.particles[i,:W], mean=mean_state, cov=covariance)
+
+        self.norm_particles()
+        
+    def update(self, vo):
+        # Update each particle according to visual odometry measurement
+        dT = 1/T
+        for i in range(self.N):
+            dy = vo.fv * dT * math.sin(self.particles[i, O])         # sin = O/H
+            dx = vo.fv * dT * math.cos(self.particles[i, O])        # cos = A/H
+            do = (vo.av) * dT
+            self.particles[i, X] += dx
+            self.particles[i, Y] += dy
+            self.particles[i, O] += do
+
+    # Assuming UWB ref is an np vector
+    def measurement(self, uwb_ref, uwb_range, hint_pos, seg_curve):
+        # Only keep points that are within uwb_range+-10cm of ref
+
+        def normal_pdf(x, mean, std_dev):
+            """Calculates the Gaussian probability density function for a given value x."""
+            exponent = -((x - mean) ** 2) / (2 * std_dev ** 2)
+            return (1 / (np.sqrt(2 * np.pi) * std_dev)) * np.exp(exponent)
+
+        for i in range(self.N):
+            pos = self.particles[i,:O]
+
+            dist_from_ref = norm(pos - uwb_ref) # Now just check how this distance falls on our UWB distribution
+            p_uwb = normal_pdf(dist_from_ref, uwb_range, UNCERTAIN)
+            self.particles[i, W] *= p_uwb
+        
+        # Now normalize s.t. all weights sum to 1
+        self.norm_particles()
+
+        print("Post-measurement")
+        self.show_particles()
+
+        print("Sum of weights before normalization")
+        # print(self.particles[:,W])
+
+    def show_particles(self):
+        if False:
+            dparticle_weights(self.particles)
+            plt.show()
+            plt.clf()
+
+    # Select the most likely particle as a weighted average of all remaining particles
+    def estimate(self):
+        self.pose = np.average(self.particles[:,:W] , weights = self.particles[:,W], axis = 0)
+        return self.pose
+    
+    def need_resample(self, seg_curvature): # Calculate effective n to determine if we need to resample
+        TURN_CEIL = 0.10745999999999996
+        curve_ratio = (seg_curvature / TURN_CEIL)**2
+    
+        weights = self.particles[:,W]
+        neff = 1. / np.sum(np.square(weights))
+        threshold = self.N/5
+
+        return neff < threshold
+    
+    def simple_resample(self, seg_curvature):
+        print("Pre-resample")
+        self.show_particles()
+        cumulative_sum = np.cumsum(self.particles[:,W])
+        cumulative_sum[-1] = 1. # avoid round-off error
+        indexes = np.searchsorted(cumulative_sum, np.random.rand(self.N))
+        # Generate N random numbers. Search for the cumulative desnities spots (particles) that are closest to that random number
+
+        # resample according to indexes
+        self.particles[:] = self.particles[indexes]
+        self.particles[:,W] = 1.0/ self.N 
+        print("Post-resample")
+        self.show_particles()
+        
 
 
 def measured_vo_to_algo2(robot_id, all_gt_pose, all_mes_vo, range_T, SLAM_T, mes_pose=None):
@@ -269,8 +369,6 @@ def measured_vo_to_algo2(robot_id, all_gt_pose, all_mes_vo, range_T, SLAM_T, mes
     dbg_start = 0
     # dbg_end = 300 * 100
     dbg_end = 120 * 100
-    
-    # dbg_view_range = range(60 * 100, 90 * 100)
     dbg_view_T = 10 * 100
 
 
