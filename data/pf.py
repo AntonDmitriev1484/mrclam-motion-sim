Fixed probability calculation I am a dumbass

data/pf.py
@@ -199,10 +199,10 @@ class ParticleFilter2:
         variances = np.array([var_x, var_y])
         covariance = np.diag(variances) # Create a cov matrix assuming no cross-correlations
 
-        for i in range(self.N):
-            self.particles[i,W] =  multivariate_normal.pdf(self.particles[i,[X,Y]], mean=mean_state, cov=covariance)
-        # We can also initialize it as a uniform, I think in the long term it doesn't make a big difference.
-
+        # for i in range(self.N):
+        #     self.particles[i,W] =  multivariate_normal.pdf(self.particles[i,[X,Y]], mean=mean_state, cov=covariance)
+        # # We can also initialize it as a uniform, I think in the long term it doesn't make a big difference.
+        self.particles[:, W] = 1/self.N
         self.norm_particles()
         
     def update(self, vo):
@@ -224,9 +224,10 @@ class ParticleFilter2:
     def measurement(self, uwb_ref, uwb_range, seg_curvature):
         TURN_CEIL = 0.10745999999999996
         curve_ratio = (seg_curvature/TURN_CEIL)
-        delta_B = 4 * (curve_ratio)
-        B = 5 + int(delta_B)
-        noise_limit = 0.05 + 0.1*(curve_ratio)
+
+        B = 5
+        noise_limit = 0.1
+
         print(f"B {B} noise_limit {noise_limit}")
         # Do we want to change B or noise limit?
         def perturb(particles, x_lim, y_lim):
@@ -242,6 +243,29 @@ class ParticleFilter2:
 
         self.norm_particles() ### Added this here don't know what it does!
 
+        center_weight = 1
+        # if curve_ratio > 0 :  center_weight /= curve_ratio
+        center_weight *= curve_ratio
+        # More likely to drift on a harder curve, so we add more searching power to our low weight particles
+
+        def noise_func(w):
+            if center_weight == 0: 
+                return 0.05
+            
+            left_bound = 0
+            right_bound = 1
+
+            m_left = noise_limit / (0+center_weight)
+            m_right = - noise_limit / (1-center_weight)
+            if (w >= center_weight):
+                return noise_limit + m_right*(w-center_weight)
+            if (w < center_weight):
+                return m_left * (w)
+
+        print(f"center_weight {center_weight}")
+        # More curve, means add more variance further out
+        # Less curve means add variance further in
+
         for i in range(self.N):
 
             v_particles = np.zeros((B, self.particles.shape[1]))
@@ -249,43 +273,32 @@ class ParticleFilter2:
 
             norm_weight = self.particles[i,W] / sum_particle_weight
 
-            noise = noise_limit * (1-norm_weight)
+            # noise = noise_func(norm_weight)
+            noise = noise_limit * (1 - norm_weight) # Default noise function
 
-            # print(f"Perturbing with noise {noise}")
+            # print(f"Center {center_weight} , Weight {norm_weight}, Perturbing with noise {noise}")
             v_particles = perturb(v_particles, noise, noise)
 
             pos = self.particles[i,[X,Y]]
             dist_from_ref = norm(pos - uwb_ref) # Now just check how this distance falls on our UWB distribution
-            p_uwb = normal_pdf(dist_from_ref, uwb_range, 0.1) # THIS WAS WRONG THE WHOLE TIME LOL!
+            # p_uwb = normal_pdf(dist_from_ref, uwb_range, 0.1)
+            # if (p_uwb > 1): print(f"p_uwb {p_uwb}")
 
-            # print(f"p_uwb {p_uwb} curve_ratio {curve_ratio}")
-            # SOmething like this
-            # max = p_uwb
-            # min = self.particles[i,W]*p_uwb
-            # delta = min + curve_ratio*(max - min)
-            # self.particles[i, W] = delta
-            self.particles[i, W] *= p_uwb
-            # print(f"curve_ratio {curve_ratio} -> delta {delta}")
+            p_uwb = normal_cdf(dist_from_ref-0.01, dist_from_ref+0.01, uwb_range, 0.1)
+            # print(f" p_uwb {p_uwb}")
+            self.particles[i, W] = p_uwb
 
-            for j in range(B):
-                pos = v_particles[j,[X,Y]]
-                dist_from_ref = norm(pos - uwb_ref) # Now just check how this distance falls on our UWB distribution
-                p_uwb = normal_pdf(dist_from_ref, uwb_range, 0.01)
+            # for j in range(B):
+            #     pos = v_particles[j,[X,Y]]
+            #     dist_from_ref = norm(pos - uwb_ref) # Now just check how this distance falls on our UWB distribution
+            #     p_uwb = normal_cdf(dist_from_ref-0.01, dist_from_ref+0.01, uwb_range, 0.1)
 
-                # max = p_uwb
-                # min = v_particles[j, W]*p_uwb
-                # delta = min + curve_ratio*(max - min)
-                # v_particles[j, W] = delta
-                v_particles[j, W] *= p_uwb
-                # If the weight of our virtual particle is greater, replace our original with it
-                if v_particles[j,W] > self.particles[i,W]: 
-                    self.particles[i,W] = v_particles[j,W]
-                    particles_replaced_count+=1
+            #     v_particles[j, W] = p_uwb
+            #     # If the weight of our virtual particle is greater, replace our original with it
+            #     if v_particles[j,W] > self.particles[i,W]: 
+            #         self.particles[i,W] = v_particles[j,W]
+            #         particles_replaced_count+=1
 
-        print(f"Replaced {particles_replaced_count}/2000 particles with virtual")
-        print(f" Weights after measurement {np.sum(self.particles[:,W])}")
-        
-        # Now normalize s.t. all weights sum to 1
         self.norm_particles()
         self.show_particles()
 
@@ -310,8 +323,9 @@ class ParticleFilter2:
         neff = 1. / np.sum(np.square(weights))
         print(f" N effective particles {neff}")
         threshold = self.N/2
+        # threshold = (self.N/2) * curve_ratio
         # At a certain point we stop re-sampling?
-        return neff < 100
+        return neff < threshold
     
     def dual_measurement(self, uwb_ref1, uwb_range1, uwb_ref2, uwb_range2):
         # Re-weigh particles to 

data/pf_algo.py
@@ -145,12 +145,12 @@ def run_pf2(robot_id, all_gt_pose, all_mes_vo, range_T, SLAM_T, mes_pose=None):
     # Segment from 1 to 1.5 minutes has problems
     # dbg_start = 40 * 100
     dbg_start = 0
-    dbg_end = 300 * 100
+    # dbg_end = 300 * 100
     # dbg_end = 120 * 100
+    dbg_end = 80 * 100
+
     dbg_view_T = 10*100
 
-    hint_imu = np.array([all_gt_pose[robot_id][0].x, all_gt_pose[robot_id][0].y, 0])
-    hint_T = 3*range_T
 
     sum_delta_angle = 0
 
@@ -189,19 +189,6 @@ def run_pf2(robot_id, all_gt_pose, all_mes_vo, range_T, SLAM_T, mes_pose=None):
             sum_delta_angle = 0
             range_count+=1
 
-            
-            if t % hint_T ==0:
-                # I suspect the estimate orientation we're outputting is wrong
-                # Because orientation has no tie to the actual state estimation
-                # So thats why the hint_imu point is starting so far out.
-                # we could do estimate[t] - estimate[t-1] to figure out the orientation to start integrating at
-                # l = 0.05
-                # vec_ori_est = [estimate[X] + l*math.cos(estimate[O]) , estimate[Y] + l*math.sin(estimate[O])]
-                # dv(estimate[[X,Y]], vec_ori_est)
-                # dv(hint_imu[[X,Y]], estimate[[X,Y]]) # This is a pass by reference apparently
-                # est_vec = estimated_poses[len(estimated_poses)-1] - estimated_poses[len(estimated_poses)-2]
-                hint_imu = np.copy(estimate)
-
         else:
             # Otherwise perform regular imu integration
             i = t%range_T
@@ -215,16 +202,13 @@ def run_pf2(robot_id, all_gt_pose, all_mes_vo, range_T, SLAM_T, mes_pose=None):
             do = (vo.av) * dT
             cur_pose = State(prev_pose.x + dx, prev_pose.y + dy, prev_pose.o + do)
 
-            delta = [vo.fv * dT * math.cos(hint_imu[[O]]), vo.fv * dT * math.sin(hint_imu[[O]]) , (vo.av) * dT]
-            hint_imu[[X,Y,O]] += delta
-
             # dp(hint_imu[[X,Y]], color='orange')
             sum_delta_angle += abs(do) # don't care about signage, just want to capture how windy this segment is
             imu_segment[i] = cur_pose
 
 
-        # if t % dbg_view_T ==0:
-        #     dparticle_weights(pf.particles)
+        if t % dbg_view_T ==0:
+            dparticle_weights(pf.particles)
     # plt.show()
 
     print(f" Out of {range_count} ranges, {resample_count} were resamples")